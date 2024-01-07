551ea2d33bbf76d5c2a7cfeb86b097b4302b5dd1
leejblack29 <leejblack29@gmail.com>
2024-01-06 20:20:00 -0500

    Write pseudocode for getComputerChoice function

---
+++ script.js
@@ -0,0 +1,8 @@
+/*
+Declare a function getComputerChoice() -- has no parameters, returns a string
+denoting the computer's choice (either "rock", "paper", or "scissors")
+
+Write and evaluate an expression which randomly returns a number between 0 and 2 (inclusive)
+
+Depending on which number is returned, return either "rock", "paper" or "scissors"
+*/
