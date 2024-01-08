61f561f4d10fafaa1fff5bbefe1acb5726f29d29
leejblack29 <leejblack29@gmail.com>
2024-01-07 21:24:35 -0500

    Write getComputerChoice function
    This function uses a switch statement to evaluate an expression
    which will always return an integer between 0 and 2, inclusive.
    
    Depending on the case, the function with return a string containing
    the computer's choice.

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-/*
-Declare a function getComputerChoice() -- has no parameters, returns a string
+
+/**
+ * Declare a function getComputerChoice() -- has no parameters, returns a string
 denoting the computer's choice (either "rock", "paper", or "scissors")
+ */
+function getComputerChoice() {  
 
-Write and evaluate an expression which randomly returns a number between 0 and 2 (inclusive)
+    // Evaluate an expression which randomly returns a number between 0 and 2 (inclusive)
+    switch (Math.floor(Math.random() * 3)) {
 
-Depending on which number is returned, return either "rock", "paper" or "scissors"
-*/
+        // Depending on which number is returned, return either "rock", "paper" or "scissors"
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break;
+        case 2:
+            return "scissors";
+            break;      
+    }
+}
+
+
+for (let i = 0; i < 30; i++) {
+    console.log(getComputerChoice());
+}
+
+
