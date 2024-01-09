d3cd798e9130c08d6d393872ff3ac16c42a5c4ea
leejblack29 <leejblack29@gmail.com>
2024-01-08 20:40:42 -0500

    Write psuedocode for playRound function

--- script.js
+++ script.js
@@ -21,4 +21,27 @@ function getComputerChoice() {
     }
 }
 
-
+/**
+ * Write a function that plays a single round of Rock Paper Scissors. 
+ * The function should take two parameters - the playerSelection and computerSelection - 
+ * and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+ * Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
+ * Account for TIES by re-playing the round.
+ * 
+ * declare a function declaration with two parameters: playerSelection and computerSelection 
+ * (We are assuming valid input for playerSelection in the form of a string "Rock", "Paper", or "Scissors" -- 
+ * spelled correctly with the only the first letter capitalized -- this will be handled in the game() function)
+ * 
+ * Create a return string for when the player wins
+ * Create a return string for when the plater loses
+ * 
+ * Check if playerSelection is equal to computerSelection -- if so, return a string saying it's a tie
+ * 
+ * Otherwise:
+ * For each possible playerSelection:
+ *      Check if the computer chose the losing option-- if so, return a string saying the player won
+ * 
+ *      Otherwise return a string saying the player lost
+ *      
+ * 
+ */
