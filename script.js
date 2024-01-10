5ad311d4c078eed354b095c06be4281819dbc9c0
leejblack29 <leejblack29@gmail.com>
2024-01-09 23:39:29 -0500

    Write pseudocode for game function

--- script.js
+++ script.js
@@ -60,3 +60,34 @@ function playRound(playerSelection, computerSelection) {
     }
   }
 }
+
+/**
+ * Write a NEW function called game(). Use the previous function inside of this one to play a best-of-five game that keeps score and reports a winner or loser at the end.
+ * 
+You have not officially learned how to “loop” over code to repeat function calls… if you already know about loops from somewhere else (or if you feel like doing some more learning) 
+feel free to use them. If not, don’t worry! Just call your playRound function 5 times in a row. Loops are covered in the next lesson.
+
+At this point you should be using console.log() to display the results of each round and the winner at the end.
+
+Use prompt() to get input from the user. Read the docs here if you need to.
+
+Feel free to re-work your previous functions if you need to. Specifically, you might want to change the return value to something more useful.
+
+Feel free to create more “helper” functions if you think it would be useful.
+
+*/
+
+//Declare a game() function
+//Declare a variable for the player's score and set to 0
+//Declare a variable for the computer's score and set to 0
+//Declare a string to store the string describing the result of each round
+//Create a while loop which loops until one of the scores is 3 
+    //Create a variable to store input
+    //Create a flag to check validity of input -- set to false
+    //Create a while loop which loops until valid input is confirmed
+        //If the input is valid, set switch the flag to true 
+        //Otherwise, prompt the user again and store the new message in input variable
+    //Call playRound() using the input, and getComputerChoice() -- store the result in the earlier created string;
+    //Determine the winner by evaluating the result string -- increment the winner, or neither if it's a tie
+    //Print the result to the console, and the current score
+//Exit the loop and print a string with the final score and result
